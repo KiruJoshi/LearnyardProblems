662e2c75c2122a10100efb85537159fdc465b3d2
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-02-24 20:07:30 +0530

    Time: 29 ms (27.93%) | Memory: 45.6 MB (16.14%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        int reach[] = new int[nums.length];
+        Arrays.fill(reach, Integer.MAX_VALUE);
+        int lastIndex = nums.length - 1;
+        reach[lastIndex] = 0;
+        for (int i = lastIndex - 1; i >= 0; i--) {
+            int value = nums[i];
+            int mn = Integer.MAX_VALUE;
+
+            for (int j = 1; j <= value && (i + j) < nums.length; j++) {
+                mn = Math.min(mn, reach[i + j]);
+
+            }
+            if (mn != Integer.MAX_VALUE) {
+                reach[i] = mn + 1;
+            }
+        }
+        return reach[0];
+    }
+}
