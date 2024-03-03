0fcd79d5c7fa29a03f634bbad636af50fda462b9
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-03-03 17:26:15 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (34.79%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,24 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int n=nums.length;
+        int result[]=new int[2];
+        int firstOccurence= searchFirst(nums,0,n-1,target);
+        int lastOccurence=searchEnd(nums,0,n-1,target);
 
-        int result[] = { -1, -1 };
-        if (nums.length == 1 && target == nums[0]) {
-            result[0]=0;
-            result[1]=0;
-            return result;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                result[0] = i;
-                break;
-            }
-        }
-        for (int i = nums.length - 1; i >=0; i--) {
-            if (nums[i] == target) {
-                result[1] = i;
-                break;
-            }
-        }
+        result[0]=firstOccurence;
+        result[1]=lastOccurence;
         return result;
+    }
+    public int searchFirst(int nums[], int start,int end,int target){
+        int first_position=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]<target){
+                start=mid+1;
+            } else if(nums[mid]>target){
+                end=mid-1;
+            } else{
+                first_position=mid;
+                end=mid-1;
+            }
+        }
+        return first_position;
+    }
+    public int searchEnd(int nums[], int start,int end,int target){
+        int last_position=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]<target){
+                start=mid+1;
+            } else if(nums[mid]>target){
+                end=mid-1;
+            } else{
+                last_position=mid;
+                start=mid+1;
+            }
+        }
+        return last_position;
     }
 }
