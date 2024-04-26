89e4c841e0dd2bd114d87573eb31a83d765439ab
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-04-26 19:14:30 +0530

    Time: 1 ms (96.30%) | Memory: 45.7 MB (22.90%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     public int minPathSumHelper(int m, int n, int[][] grid, int[][] dp){
         
-        if(n>= grid[0].length || m>=grid.length){
+        if(n<0 || m<0){
         return 100000;//10^5
        }
-        if(n==grid[0].length-1 && m==grid.length-1){
+        if(n==0 && m==0){
             return grid[m][n];
        } 
         if(dp[m][n]!=-1){
         return dp[m][n];
        }
 
-       int count1 = minPathSumHelper(m+1, n, grid, dp);//down
-       int count2 = minPathSumHelper(m, n+1, grid, dp);//right
+       int count1 = minPathSumHelper(m-1, n, grid, dp);//down
+       int count2 = minPathSumHelper(m, n-1, grid, dp);//right
 
         return dp[m][n] = grid[m][n] + Math.min(count1, count2);
     }
@@ -22,6 +22,6 @@ class Solution {
         int dp[][]=new int[m+1][n+1]; 
         for(int[] row : dp)
             Arrays.fill(row, -1);
-        return  minPathSumHelper(0, 0, grid,dp) ;    
+        return  minPathSumHelper(m-1, n-1, grid,dp) ;    
     }
 }
