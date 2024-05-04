912f7727b4d7fa32ed7b08fa30fce324a86d061c
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-05-04 20:26:10 +0530

    Time: 1 ms (59.66%) | Memory: 42.7 MB (43.12%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,20 +1,17 @@
 class Solution {
-    public static void helper(List<List<Integer>> ans, List<Integer> temp, int[] nums, int i){
-        if(i == nums.length){
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
-        //we have to choose either to pick it or not
-        temp.add(nums[i]);
-        helper(ans,temp,nums,i+1);
-        //we hv choosen not to pick this element;
-        temp.remove(temp.size()-1);
-        helper(ans,temp,nums,i+1);
-    }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        helper(ans,temp,nums,0);
-        return ans;
+     List<List<Integer>> result = new ArrayList<>();
+     int n = nums.length;
+      for(int i=0; i<(1 << n); i++) {
+      List<Integer> temp = new ArrayList<>();
+        for(int j = 0; j<n; j++){
+        
+            if((i & (1<<j)) != 0){
+                temp.add(nums[j]);
+                }
+            }
+              result.add(new ArrayList<>(temp));
+        }
+        return result;
     }
 }
