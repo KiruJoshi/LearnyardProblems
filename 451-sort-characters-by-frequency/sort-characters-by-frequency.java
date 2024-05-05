cb37b76ca91c18de42047af99ca820207ae957aa
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-05-05 11:28:54 +0530

    Time: 21 ms (23.99%) | Memory: 45.1 MB (78.53%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -6,13 +6,13 @@ class Solution {
         map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
       } 
 
-      // Sorting
+      // Sorting add keys of map in list and then we sort list on the basis of keys
 	List<Character> chars = new ArrayList(map.keySet());
 	Collections.sort(chars, (a, b) -> (map.get(b) - map.get(a)));
 
      
       System.out.println(map.keySet());
-      for (Object c : chars) {
+      for (Character c : chars) {
 		for (int i = 0; i < map.get(c); i++) {
 			result.append(c);
 		}
