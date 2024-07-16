2b86928265ae0713588dd8d4dbde65acc4f38536
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-07-16 21:20:13 +0530

    Time: 15 ms (38.60%) | Memory: 45.5 MB (87.09%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,45 +1,35 @@
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-
-        int arr[] = new int[k];
-       
-        int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-
-        // Convert HashMap to a List of Map.Entry objects
-        List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
-
-        // Sort the list based on values
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-        int j=0;
-        for (int i = list.size()-1; i >= 0; i--) {
-            arr[j] = list.get(i).getKey();
-            j++;
-            if(j==k){
-                break;
-            }
-        }
-        return arr;
+class Pair{
+    int ele;
+    int freq;
+    Pair(int ele, int freq){
+        this.ele=ele;
+        this.freq=freq;
     }
 }
-/*
- * nums=[1,1,1,1,2,2,3,3,3,4], k=2
- * ans=1 , 3
- * 1-4
- * 2-2
- * 3-3
- * 4-1
- * 
- * sort
- * 1-4
- * 3-3
- * 2-2
- * 4-1
- */
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+    HashMap<Integer, Integer> map=new HashMap<>();
+    List<Pair> list=new ArrayList<>();
+    int result[]=new int[k];
+    for(int i=0;i<nums.length;i++){
+        map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+    }  
+    for(Map.Entry<Integer, Integer> entry:map.entrySet()){
+        Pair pair=new Pair(entry.getKey(),entry.getValue());
+        list.add(pair);
+    }
+    Collections.sort(list,(a,b)->b.freq-a.freq);
+    // for(Pair pair:list){
+    //     System.out.println(pair.ele+" - "+pair.freq);
+    // }
+    for(int i=0;i<list.size() && i<k;i++){
+        result[i]=list.get(i).ele;
+    }
+    
+
+
+     
+ 
+    return result;
+    }
+}
