fbc83800deca4351da762627a1994847df71dcf7
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-07-27 22:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (6.94%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,29 +1,40 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
-        ListNode dummy =new ListNode(0);
-        ListNode current=dummy;
-        while(l1!=null && l2!=null){
-            if(l1.val<=l2.val){
-                current.next=l1;
-                l1=l1.next;
-            } else{
-                current.next=l2;
-                l2=l2.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current1 = list1;
+        ListNode current2 = list2;
+        ListNode ptr = dummy, temp = null;
+        while (current1 != null && current2 != null) {
+            if (current1.val <= current2.val) {
+                ptr.next = new ListNode(current1.val);
+                ptr = ptr.next;
+                current1 = current1.next;
+            } else {
+                ptr.next = new ListNode(current2.val);
+                ptr = ptr.next;
+                current2 = current2.next;
             }
-            current=current.next;
         }
-         current.next=l1!=null?l1:l2;
-            return dummy.next;
+        while (current1 != null) {
+            ptr.next = new ListNode(current1.val);
+            ptr = ptr.next;
+            current1 = current1.next;
+        }
+        while (current2 != null) {
+            ptr.next = new ListNode(current2.val);
+            ptr = ptr.next;
+            current2 = current2.next;
+        }
+        return dummy.next;
     }
 }
