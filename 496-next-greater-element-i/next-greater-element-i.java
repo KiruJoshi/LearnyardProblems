9a1dc06c80f1729c6e2814ad6c067fe873b520a8
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-08-10 17:38:52 +0530

    Time: 3 ms (92.14%) | Memory: 44.2 MB (22.13%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,25 +1,34 @@
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
-     int n=nums1.length;
-      int result[]=new int[n]; 
-      Arrays.fill(result, -1);
-       for(int i=0;i<nums1.length;i++){
-      int j=0,ans=-1;
-        for(;j<nums2.length;j++){
-            if(nums1[i]==nums2[j]){
-                break;
+        // HashMap to store the next greater element for each number in nums2
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // Stack to help find the next greater element
+        Stack<Integer> st = new Stack<>();
+        
+        // Traverse nums2 from right to left
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            // Maintain elements in the stack where the top is always the next greater element
+            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                st.pop();//if it is small then no need bcz we need larger element
             }
-         }
-    
-       for(;j<nums2.length;j++){
-            if(nums1[i]<nums2[j]){
-               ans=nums2[j];
-                break;
-            }
-         }
-        result[i]=ans;
-       }
-    return result;
+            // If the stack is empty, there's no greater element; otherwise, the top of the stack is the next greater element
+            if (st.isEmpty())
+                map.put(nums2[i], -1);
+            else
+                map.put(nums2[i], st.peek());
+            
+            // Push the current element to the stack
+            st.push(nums2[i]);
+        }
+        
+        // Prepare the result array
+        int[] result = new int[nums1.length];
+        for (int j = 0; j < nums1.length; j++) {
+            // Get the next greater element from the map
+            result[j] = map.get(nums1[j]);
+        }
+        
+        return result;
     }
 }
