4e8baebb22e8df0dc472a6379a81ab82c0855430
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-08-11 17:47:26 +0530

    Time: 2 ms (90.11%) | Memory: 47.1 MB (48.82%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,31 +1,25 @@
 class Solution {
-    public int binarySearch(int start,int end,int arr[], int target ){
-        int ans=-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(arr[mid]>target){
-                end=mid-1;
-            } else if(arr[mid]<target){
-                start=mid+1;
-            } else{
-                ans=mid;
-                end=mid-1;
+    public int[] twoSum(int[] numbers, int target) {
+       int N = numbers.length;//4
+        int i = 0;
+        int j = N-1;//3
+        int ans[] = new int[2];
+        while (i < j)//(0<1)
+        {
+            if (numbers[i] + numbers[j] == target)
+            {
+                ans[0] = i+1;//1
+                ans[1] = j+1;//2
+                break;
+            }
+            if (numbers[i] + numbers[j] > target) 
+            {
+                j-- ;
+            } else 
+            {
+                i++ ;
             }
         }
         return ans;
-    }
-    public int[] twoSum(int[] numbers, int target) {
-        int result[]=new int[2];
-        int n=numbers.length;
-        for(int i=0;i<numbers.length;i++){
-
-              int firstOcc=binarySearch((i+1),(n-1),numbers,target-numbers[i]);
-              if(firstOcc!=-1){
-                  result[0]=i+1;
-                  result[1]=firstOcc+1;
-              }
-        }
-     
-        return result;
     }
 }
