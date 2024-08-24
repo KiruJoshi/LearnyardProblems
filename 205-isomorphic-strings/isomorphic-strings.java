46f0249f37eeb0978e7d896a1198594ee970eb33
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-08-24 19:19:33 +0530

    Time: 23 ms (11.30%) | Memory: 42.2 MB (70.56%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,27 +1,19 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+      HashMap<Character, Character> sTot=new HashMap<>();
+      HashMap<Character, Character> tTos=new HashMap<>();
+      for(int i=0;i<s.length();i++){
+        char charS=s.charAt(i);
+        char charT=t.charAt(i);
 
-      
-    HashMap<Character, Character> sToT = new HashMap<>();
-    HashMap<Character, Character> tToS = new HashMap<>();
+        if(sTot.containsKey(charS) && sTot.get(charS)!=charT)
+          return false;
+        if(tTos.containsKey(charT) && tTos.get(charT)!=charS)
+          return false; 
+        sTot.put(charS, charT);
+        tTos.put(charT, charS);
 
-    for (int i = 0; i < s.length(); i++) {
-        char charS = s.charAt(i);
-        char charT = t.charAt(i);
-
-        // Check if the mapping is consistent
-        if (sToT.containsKey(charS) && sToT.get(charS) != charT) {
-            return false;
-        }
-        if (tToS.containsKey(charT) && tToS.get(charT) != charS) {
-            return false;
-        }
-
-        // Create mappings
-        sToT.put(charS, charT);
-        tToS.put(charT, charS);
+      } 
+       return true;
     }
-
-    return true;
-   }
 }
