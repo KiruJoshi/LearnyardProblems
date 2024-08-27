42a346f7365a1cf71c6bb7006aa369563e2d22ae
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-08-27 22:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (19.05%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,15 +1,15 @@
 class Solution {
     public int search(int[] nums, int target) {
-   int start = 0;
+        int start = 0;
         int end = nums.length - 1;
-        
+
         while (start <= end) {
             int mid = (start + end) / 2;
-            
+
             if (nums[mid] == target) {
                 return mid;
             }
-            
+
             // The tricky part is to handle the duplicates
             if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
                 start++;
@@ -28,7 +28,7 @@ class Solution {
                 }
             }
         }
-        
+
         return -1;
     }
 }
