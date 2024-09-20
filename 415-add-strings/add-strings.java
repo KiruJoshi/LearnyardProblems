5f10a00a9d8af6da2c175a1266b59ec72579d412
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-09-20 21:22:12 +0530

    Time: 3 ms (38.08%) | Memory: 42.2 MB (87.58%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,32 +1,30 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-     
-         int i = num1.length() - 1;
-        int j = num2.length() - 1;
-        StringBuilder ans = new StringBuilder();
-        int sum = 0;
-       
-        while(i>=0 && j>=0){
-            sum+=(num1.charAt(i)-'0')+(num2.charAt(j)-'0');//42
-            ans.append((char)(sum%10+'0'));//ans.append(2)
-            sum/=10;//4
-            i--;
-            j--;
-        } while(i>=0){
-            sum+=(num1.charAt(i)-'0');
-            ans.append((char)(sum%10+'0'));
-            sum=sum/10;
-            i--;
-        }while(j>=0){
-            sum+=(num2.charAt(j)-'0');
-            ans.append((char)(sum%10+'0'));
-            sum=sum/10;
-            j--;
-        }
+       StringBuilder result = new StringBuilder(); 
+       int i = num1.length()-1;
+       int j = num2.length()-1;
+       int sum=0;
+       while(i>=0 && j>=0){
+        sum+=(num1.charAt(i)-'0')+(num2.charAt(j)-'0');
+        result.append((char)(sum%10+'0'));
+        sum/=10;
+        i--; j--;
+       } 
+       while(i>=0){
+        sum+=(num1.charAt(i)-'0');
+        result.append((char)(sum%10+'0'));
+        sum/=10;
+        i--; 
+       } 
+       while(j>=0){
+        sum+=(num2.charAt(j)-'0');
+        result.append((char)(sum%10+'0'));
+        sum/=10;
+        j--;
+       } 
         if(sum>0){
-            ans.append((char)(sum+'0'));
+            result.append((char)(sum+'0'));
         }
-   
-        return ans.reverse().toString();
+        return result.reverse().toString();
     }
 }
