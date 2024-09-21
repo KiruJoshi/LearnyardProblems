81171e48ddcd939f9a397bc34d65124f7a2efa69
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-09-21 10:17:02 +0530

    Time: 21 ms (20.97%) | Memory: 45.2 MB (82.43%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,22 +1,17 @@
 class Solution {
     public String frequencySort(String s) {
-      StringBuilder result = new StringBuilder();
-      LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
-      for(int i = 0; i<s.length(); i++){
-        map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
-      } 
-
-      // Sorting add keys of map in list and then we sort list on the basis of keys
-	List<Character> chars = new ArrayList(map.keySet());
-	Collections.sort(chars, (a, b) -> (map.get(b) - map.get(a)));
-
-     
-      System.out.println(map.keySet());
-      for (Character c : chars) { //travesre on list
-		for (int i = 0; i < map.get(c); i++) {
-			result.append(c);
-		}
-	}
-      return result.toString();
+     StringBuilder str = new StringBuilder();
+     LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+     for(int i=0;i<s.length();i++){
+        map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+     }  
+     List<Character> list = new ArrayList<>(map.keySet());
+     Collections.sort(list,(a,b)->(map.get(b)-map.get(a)));
+     for(Character c:list){
+        for(int i=0;i<map.get(c); i++){
+            str.append(c);
+        }
+     }
+     return str.toString();
     }
 }
