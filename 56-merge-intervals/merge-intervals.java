83d7ce7f4d8813f07d1f6a936b2c2c0c7d8a38ad
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-10-28 21:20:19 +0530

    Time: 9 ms (51.49%) | Memory: 46.4 MB (56.31%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,54 +1,44 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
-       // Arrays.sort(intervals, (a,b) -> (a[0]-b[0]));//o(nlog(n))
-        //int i = 0, j = 1;//o(1)
-        // while(j < n){//o(n)
-        //     if(intervals[j][0] <= intervals[i][1]){
-        //         intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
-        //     } else {
-        //         i++;
-        //         intervals[i][0] = intervals[j][0];
-        //         intervals[i][1] = intervals[j][1];
-        //     }
-        //     j++;
-        // }
-        // int ans[][] = new int[i+1][2];//o(1)
-        // for(int k=0; k<=i; k++){//o(n)
-        //     ans[k][0] = intervals[k][0];
-        //     ans[k][1] = intervals[k][1];
-        // }
-        // return ans;
-        
-        int n = intervals.length;//o(1)
+        int n = intervals.length;// o(1)
         int[][] result = new int[n][2];
-        
-        for (int i = 0; i <n; i++) {
+
+        for (int i = 0; i < n; i++) {
             result[i][0] = intervals[i][0];
             result[i][1] = intervals[i][1];
         }
         Arrays.sort(result, (a, b) -> (a[0] - b[0]));
         ArrayList<int[]> list = new ArrayList<>();
-         int[][] ans = new int[list.size()][2];
-         int i = 0, j = 1;//o(1)
-        while (j < n ) {
-            if (result[i][1] >= result[j][0]) {// overlapping condition
+        int ans[][] = new int[list.size()][2];
+     
+        int i = 0, j = 1;
+        /**
+        (1,7) (2,6)
+        (1,6)
+         */
+        while (j < n) {
+            if (result[i][1] >= result[j][0]) {
                 result[i][1] = Math.max(result[i][1], result[j][1]);
             } else {
-                // Add the merged interval to the list
                 list.add(new int[] { result[i][0], result[i][1] });
-                i = j;// i=2
+                i = j;
             }
             j++;
         }
-       // Add the last merged interval to the list
         list.add(new int[] { result[i][0], result[i][1] });
-
-        // Initialize ans array with the correct size
         ans = new int[list.size()][2];
-         list.toArray(ans);
+        list.toArray(ans);
+
         return ans;
     }
 }
-//Time Complexity-o(nlog(n))
-//space complexity = o(n)
+
+/**
+1,3 2,6 5,10 15,18
+(1,6),(5,10), (15,18)
+(1,10)(15,18)
+2,6
+ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+ -----(1,3) (1,6)
+    --------(2,6)
+ */
