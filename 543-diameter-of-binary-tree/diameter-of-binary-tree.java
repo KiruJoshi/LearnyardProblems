8c6509a0ef5141a909507f664e2c07c080195e7b
KiruJoshi <94553334+KiruJoshi@users.noreply.github.com>
2024-11-24 15:47:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (73.91%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -1,39 +1,30 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    int max = Integer.MIN_VALUE;
-
-    public int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    int diameter=0;
+    public int height(TreeNode root){
+        if(root==null) 
+          return 0;
+        int left_height=height(root.left);
+        int right_height=height(root.right);
+        diameter=Math.max(diameter,(left_height+right_height));
+        return (1+Math.max(left_height,right_height));
     }
-
-    public void diameterHelper(TreeNode root) {
-        if (root == null)
-            return;
-        diameterHelper(root.left);
-        diameterHelper(root.right);
-        int max_left = maxDepth(root.left);
-        int max_right = maxDepth(root.right);
-        max = Math.max((max_left + max_right), max);
-    }
-
     public int diameterOfBinaryTree(TreeNode root) {
-         diameterHelper(root);
-         return max;
+        height(root);
+        return diameter;
     }
 }
